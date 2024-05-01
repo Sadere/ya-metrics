1d74fe9386574e8530d7cb4cbb455f20c502d0c8
sample <sasa.mie96@gmail.com>
2024-05-01 17:56:24 +0500

    added missing handler tests

--- internal/server/handlers_test.go
+++ internal/server/handlers_test.go
@@ -1,16 +1,179 @@
 package server
 
 import (
+	"errors"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/Sadere/ya-metrics/internal/common"
 	"github.com/Sadere/ya-metrics/internal/server/storage"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestServer_updateHandlers(t *testing.T) {
+type TestStorage struct{}
+
+func (ts *TestStorage) Get(common.MetricType, string) (common.Metrics, error) {
+	return common.Metrics{}, errors.New("Get() error")
+}
+func (ts *TestStorage) Set(common.Metrics) error {
+	return errors.New("Set() error")
+}
+
+func (ts *TestStorage) GetData() (map[string]common.Metrics, error) {
+	return nil, errors.New("GetData() error")
+}
+func (ts *TestStorage) SetData(map[string]common.Metrics) error {
+	return errors.New("SetData() error")
+}
+
+func TestHandlers_text(t *testing.T) {
 	server := Server{repository: storage.NewMemRepository()}
+	server.InitLogging()
+
+	router := server.setupRouter()
+
+	type want struct {
+		contentType string
+		statusCode  int
+		body string
+	}
+	tests := []struct {
+		name    string
+		request string
+		method  string
+		want    want
+	}{
+		{
+			name:    "save gauge",
+			request: "/update/gauge/gaugeMetric/100.35",
+			method:  http.MethodPost,
+			want: want{
+				contentType: "text/plain",
+				statusCode:  http.StatusOK,
+			},
+		},
+		{
+			name:    "save counter",
+			request: "/update/counter/counterMetric/400",
+			method:  http.MethodPost,
+			want: want{
+				contentType: "text/plain",
+				statusCode:  http.StatusOK,
+			},
+		},
+		{
+			name:    "wrong request",
+			request: "/test",
+			method:  http.MethodPost,
+			want: want{
+				contentType: "text/plain",
+				statusCode:  http.StatusNotFound,
+			},
+		},
+		{
+			name:    "save wrong type",
+			request: "/update/invalid/someMetric/100",
+			method:  http.MethodPost,
+			want: want{
+				contentType: "text/plain",
+				statusCode:  http.StatusBadRequest,
+			},
+		},
+		{
+			name:    "invalid gauge value",
+			request: "/update/gauge/gaugeMetric/invalid",
+			method:  http.MethodPost,
+			want: want{
+				contentType: "",
+				statusCode:  http.StatusBadRequest,
+			},
+		},
+		{
+			name:    "invalid counter value",
+			request: "/update/counter/counterMetric/invalid",
+			method:  http.MethodPost,
+			want: want{
+				contentType: "",
+				statusCode:  http.StatusBadRequest,
+			},
+		},
+		{
+			name:    "get valid gauge",
+			request: "/value/gauge/gaugeMetric",
+			method:  http.MethodGet,
+			want: want{
+				contentType: "text/plain",
+				statusCode:  http.StatusOK,
+				body: "100.35",
+			},
+		},
+		{
+			name:    "get valid counter",
+			request: "/value/counter/counterMetric",
+			method:  http.MethodGet,
+			want: want{
+				contentType: "text/plain",
+				statusCode:  http.StatusOK,
+				body: "400",
+			},
+		},
+		{
+			name:    "get unknown metric type",
+			request: "/value/invalid/someMetric",
+			method:  http.MethodGet,
+			want: want{
+				contentType: "text/plain",
+				statusCode:  http.StatusNotFound,
+			},
+		},
+		{
+			name:    "get invalid gauge",
+			request: "/value/gauge/notExists",
+			method:  http.MethodGet,
+			want: want{
+				contentType: "text/plain",
+				statusCode:  http.StatusNotFound,
+			},
+		},
+		{
+			name:    "get invalid counter",
+			request: "/value/counter/notExists",
+			method:  http.MethodGet,
+			want: want{
+				contentType: "text/plain",
+				statusCode:  http.StatusNotFound,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(tt.method, tt.request, nil)
+			w := httptest.NewRecorder()
+
+			router.ServeHTTP(w, request)
+
+			result := w.Result()
+
+			defer result.Body.Close()
+
+			assert.Equal(t, tt.want.statusCode, result.StatusCode)
+			assert.Contains(t, result.Header.Get("Content-Type"), tt.want.contentType)
+
+			if len(tt.want.body) > 0 {
+				resultBody, err := io.ReadAll(result.Body)
+				assert.Nil(t, err)
+
+				assert.Equal(t, tt.want.body, string(resultBody))
+			}
+		})
+	}
+}
+
+
+func TestHandler_errorStorage(t *testing.T) {
+	server := Server{repository: &TestStorage{}}
 	server.InitLogging()
 
 	router := server.setupRouter()
@@ -26,30 +189,21 @@ func TestServer_updateHandlers(t *testing.T) {
 		want    want
 	}{
 		{
-			name:    "save gauge",
-			request: "/update/gauge/someMetric/100",
+			name:    "test gauge",
+			request: "/update/gauge/someMetric/200",
 			method:  http.MethodPost,
 			want: want{
-				contentType: "text/plain",
-				statusCode:  http.StatusOK,
+				contentType: "",
+				statusCode:  http.StatusBadRequest,
 			},
 		},
 		{
-			name:    "save counter",
-			request: "/update/counter/someMetric/100",
+			name:    "test counter",
+			request: "/update/counter/someMetric/300",
 			method:  http.MethodPost,
 			want: want{
-				contentType: "text/plain",
-				statusCode:  http.StatusOK,
-			},
-		},
-		{
-			name:    "wrong request",
-			request: "/test",
-			method:  http.MethodPost,
-			want: want{
-				contentType: "text/plain",
-				statusCode:  http.StatusNotFound,
+				contentType: "",
+				statusCode:  http.StatusBadRequest,
 			},
 		},
 	}
