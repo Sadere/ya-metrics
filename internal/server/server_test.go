891bb7a0116adff969b0e0d92663afb9e6f71155
sample <sasa.mie96@gmail.com>
2024-06-24 22:21:17 +0300

    add examples for server endpoints

---
+++ internal/server/server_test.go
@@ -0,0 +1,65 @@
+package server_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Example() {
+	type metric struct {
+		ID    string
+		MType string
+		Delta *int64
+		Value *float64
+	}
+
+	d := int64(100)
+	m := metric{
+		ID:    "exampleCounter",
+		MType: "counter",
+		Delta: &d,
+	}
+	postBody, _ := json.Marshal(m)
+
+	// Пример отправки метрики
+	http.Post("http://example.com:8080/update/", "application/json", bytes.NewBuffer(postBody))
+
+	// Пример получения значения метрики
+	var resultMetric metric
+
+	resp, err := http.Post("http://example.com:8080/value/", "application/json", bytes.NewBuffer(postBody))
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	json.Unmarshal(body, &resultMetric)
+
+	// resultMetric содержит структуру метрики
+
+	// Пример отправки нескольких метрик
+	var metrics []metric
+
+	v := 123.456
+	for i := 0; i < 10; i++ {
+		metrics = append(metrics, metric{
+			ID:    fmt.Sprintf("exampleGauge%d", i),
+			MType: "gauge",
+			Value: &v,
+		})
+	}
+
+	postBody, _ = json.Marshal(metrics)
+
+	http.Post("http://example.com:8080/updates/", "application/json", bytes.NewBuffer(postBody))
+
+}
